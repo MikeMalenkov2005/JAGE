f92b73cb17244d6bec433bf0a494b07c19e39ef4
MikeMalenkov2005 <mike.malenkov@gmail.com>
2023-07-11 21:50:12 +0300

    Refactored some code

--- src/main/java/com/github/MikeMalenkov2005/jage/enums/GLSLStruct.java
+++ src/main/java/com/github/MikeMalenkov2005/jage/enums/GLSLStruct.java
@@ -1,17 +1,23 @@
 package com.github.MikeMalenkov2005.jage.enums;
 
 public enum GLSLStruct {
-    SCALAR,
-    VECTOR_2D,
-    VECTOR_3D,
-    VECTOR_4D,
-    MATRIX_2X2,
-    MATRIX_2X3,
-    MATRIX_2X4,
-    MATRIX_3X2,
-    MATRIX_3X3,
-    MATRIX_3X4,
-    MATRIX_4X2,
-    MATRIX_4X3,
-    MATRIX_4X4
+    SCALAR(1),
+    VECTOR_2D(2),
+    VECTOR_3D(3),
+    VECTOR_4D(4),
+    MATRIX_2X2(4),
+    MATRIX_2X3(6),
+    MATRIX_2X4(8),
+    MATRIX_3X2(6),
+    MATRIX_3X3(9),
+    MATRIX_3X4(12),
+    MATRIX_4X2(8),
+    MATRIX_4X3(12),
+    MATRIX_4X4(16);
+
+    public final int size;
+
+    GLSLStruct(int size) {
+        this.size = size;
+    }
 }
